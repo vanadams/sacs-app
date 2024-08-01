70224e031e0aabec5662f5e57475b8e3219c086c
Van Adams <vanadams.luciano@gmail.com>
2024-08-01 12:40:02 +0800

    Updated title

--- sac-app/resources/views/layouts/app.blade.php
+++ sac-app/resources/views/layouts/app.blade.php
@@ -3,18 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Social Amelioration Card (SAC)</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="{{ url('/') }}">
-            {{ config('app.name', 'Laravel') }}
-        </a>
         <div class="collapse navbar-collapse">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('beneficiaries.index') }}">SACs</a>
+                    <a class="nav-link" href="{{ route('beneficiaries.index') }}">Social Amelioration Card (SAC)</a>
                 </li>
             </ul>
         </div>
