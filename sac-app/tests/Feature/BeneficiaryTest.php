41dc9c27c66def1c4ff68c95891e5a91f89a5812
Van Adams <vanadams.luciano@gmail.com>
2024-08-01 15:02:45 +0800

    Updated Beneficiary Test

--- sac-app/tests/Feature/BeneficiaryTest.php
+++ sac-app/tests/Feature/BeneficiaryTest.php
@@ -47,12 +47,59 @@ class BeneficiaryTest extends TestCase
             'uri_ng_katutubo' => null,
             'others' => 0,
             'uri_ng_others' => null,
+            'family_members' => json_encode([
+                [
+                    'last_name' => 'Rombawa',
+                    'first_name' => 'Anj',
+                    'middle_name' => '',
+                    'extension_name' => '',
+                    'relasyon_sa_benepisyaryo' => 'Partner',
+                    'petsa_ng_kapanganakan' => '1999-08-01',
+                    'kasarian' => 'babae',
+                    'trabaho' => 'AAII',
+                    'sektor' => 'Buntis',
+                    'kondisyon_ng_kalusugan' => ''
+                ]
+            ])
         ];
 
-        $response = $this->post(route('beneficiaries.store'), $data);
+        $response = $this->withoutMiddleware(\App\Http\Middleware\VerifyCsrfToken::class)->post(route('beneficiaries.store'), $data);
+        $response->assertStatus(302);
 
-        $this->assertDatabaseHas('beneficiaries', $data);
+        $this->assertDatabaseHas('beneficiaries', [
+            'last_name' => 'Espera',
+            'first_name' => 'Jocian Keith',
+            'middle_name' => 'Mijares',
+            'extension_name' => 'Jr',
+            'kasarian' => 'lalaki',
+            'tirahan' => 'Santa Monica, PPC',
+            'kalye' => 'Employee\'s Village',
+            'barangay' => 'Santa Monica',
+            'lungsod_bayan' => 'Puerto Princesa City',
+            'probinsya' => 'Palawan',
+            'rehiyon' => 'Mimaropa',
+            'petsa_ng_kapanganakan' => '1995-07-02',
+            'sektor' => 'Solo Parent',
+            'kondisyon_ng_kalusugan' => null,
+            'bene_ng_uct' => 0,
+            'bene_ng_4ps' => 0,
+            'katutubo' => 0,
+            'uri_ng_katutubo' => null,
+            'others' => 0,
+            'uri_ng_others' => null,
+        ]);
 
-        $response->assertRedirectToRoute('beneficiaries.index');
+        $this->assertDatabaseHas('beneficiary_family_members', [
+            'last_name' => 'Rombawa',
+            'first_name' => 'Anj',
+            'middle_name' => '',
+            'extension_name' => '',
+            'relasyon_sa_benepisyaryo' => 'Partner',
+            'petsa_ng_kapanganakan' => '1999-08-01',
+            'kasarian' => 'babae',
+            'trabaho' => 'AAII',
+            'sektor' => 'Buntis',
+            'kondisyon_ng_kalusugan' => ''
+        ]);
     }
 }
