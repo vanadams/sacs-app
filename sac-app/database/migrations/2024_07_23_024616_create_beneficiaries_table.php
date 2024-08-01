7731c54a4a0791ead4018c9671168d1520f07aea
Van Adams <vanadams.luciano@gmail.com>
2024-08-01 12:37:43 +0800

    Updated beneficiaries mirgration

--- sac-app/database/migrations/2024_07_23_024616_create_beneficiaries_table.php
+++ sac-app/database/migrations/2024_07_23_024616_create_beneficiaries_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('beneficiaries', function (Blueprint $table) {
             $table->id();
-            $table->string('pangalan');
+            $table->string('last_name');
+            $table->string('first_name');
+            $table->string('middle_name')->nullable();
+            $table->string('extension_name')->nullable();
             $table->string('kasarian');
             $table->string('tirahan');
             $table->string('kalye')->nullable();
