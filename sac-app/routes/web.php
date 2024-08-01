cd5218edc786aba8e2083d975e2dfc513a649bbd
Van Adams <vanadams.luciano@gmail.com>
2024-08-01 12:37:17 +0800

    Updated web routes

--- sac-app/routes/web.php
+++ sac-app/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BeneficiaryController;
+use App\Http\Controllers\BeneficiaryFamilyMemberController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,12 +19,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::resource('sacs', BeneficiaryController::class);
-
 Route::get('/sacs/beneficiaries', [BeneficiaryController::class, 'index'])->name('beneficiaries.index');
 Route::get('/sacs/beneficiaries/create', [BeneficiaryController::class, 'create'])->name('beneficiaries.create');
 Route::post('/sacs/beneficiaries', [BeneficiaryController::class, 'store'])->name('beneficiaries.store');
-Route::get('/sacs/beneficiaries/{/beneficiary}', [BeneficiaryController::class, 'show'])->name('beneficiaries.show');
+Route::get('/sacs/beneficiaries/{beneficiary}', [BeneficiaryController::class, 'show'])->name('beneficiaries.show');
 Route::get('/sacs/beneficiaries/{beneficiary}/edit', [BeneficiaryController::class, 'edit'])->name('beneficiaries.edit');
 Route::put('/sacs/beneficiaries/{beneficiary}', [BeneficiaryController::class, 'update'])->name('beneficiaries.update');
 Route::delete('/sacs/beneficiaries/{beneficiary}', [BeneficiaryController::class, 'destroy'])->name('beneficiaries.destroy');
+
+Route::get('/sacs/beneficiaries/{beneficiary}/family-member/create', [BeneficiaryFamilyMemberController::class, 'create'])->name('family_members.create');
+Route::post('/sacs/beneficiaries/{beneficiary}/family-member', [BeneficiaryFamilyMemberController::class, 'store'])->name('family_members.store');
+Route::get('/sacs/beneficiaries/{beneficiary}/family-member/{familyMember}/edit', [BeneficiaryFamilyMemberController::class, 'edit'])->name('family_members.edit');
+Route::put('/sacs/beneficiaries/{beneficiary}/family-member/{familyMember}', [BeneficiaryFamilyMemberController::class, 'update'])->name('family_members.update');
+Route::delete('/sacs/beneficiaries/{beneficiary}/family-member/{familyMember}', [BeneficiaryFamilyMemberController::class, 'destroy'])->name('family_members.destroy');
